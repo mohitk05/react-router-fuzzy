e9672f94f6d9ea4e0d2069602c93b697650ce21c
Mohit Karekar <karekar.mohit@gmail.com>
2019-04-04 19:48:36 +0530

    url fix

--- example/src/history.js
+++ example/src/history.js
@@ -1,5 +1,7 @@
 import { createBrowserHistory } from 'history'
 
-let history = createBrowserHistory()
+let history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL,
+})
 
 export default history
